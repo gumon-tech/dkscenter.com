65c23e85fd2c35f58e0401258f18255d424095ec
komphet <komphet.me@gmail.com>
2024-02-18 16:59:44 +0700

    Fix bug build nextjs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
+
+module.exports = nextConfig;
