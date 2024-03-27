87cc84c5a54b1b8987d1236cdd5008ec20f6107b
dinotong <dinotongo@gmail.com>
2024-03-27 11:51:40 +0700

    add html lang

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Link from "next/link";
+import nextI18nextConfig from "../next-i18next.config";
 class MyDocument extends Document {
   render() {
+    const currentLocale = this.props.__NEXT_DATA__.query.locale || nextI18nextConfig.i18n.defaultLocale
+
     return (
-      <Html>
+      <Html lang={currentLocale}>
         <Head>
           <Link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
