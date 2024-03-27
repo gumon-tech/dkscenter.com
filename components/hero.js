3e771ef2484594d17eeb6d9bc53a2e46a33833b4
dinotong <dinotongo@gmail.com>
2024-03-27 17:45:34 +0700

    add i18n to hero components

--- components/hero.js
+++ components/hero.js
@@ -3,18 +3,18 @@ import Container from "./container";
 import heroImg from "../public/img/assistant1.jpg";
 
 const Hero = (props) => {
+  const i18next = props.i18next;
+  const { t, i18n } = i18next;
   return (
     <>
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Digital Knowledge Sharing Center
+              {t("hero-1")}
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              DKS - Connecting digital communities and tech enthusiasts. We
-              facilitate collaboration, host joint events, and continuously
-              update knowledge through community exchanges.
+              {t("hero-2")}
             </p>
             {props.children}
           </div>
