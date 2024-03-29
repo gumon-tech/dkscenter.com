e30c33ebc32b6a1d20c51661382bbb901bf5e4d3
dinotong <dinotongo@gmail.com>
2024-03-29 16:36:47 +0700

    add  Head meta data

--- pages/[locale]/index.js
+++ pages/[locale]/index.js
@@ -8,6 +8,8 @@ import Link from "/components/link";
 import { useTranslation } from "next-i18next";
 import { makeStaticProps } from "/lib/getStatic";
 import { getStaticPaths } from "/lib/getStatic";
+import { useRouter } from "next/router";
+
 
 const getStaticProps = makeStaticProps(["home"]);
 export { getStaticPaths, getStaticProps };
@@ -16,12 +18,52 @@ const Home = () => {
   const i18next = useTranslation("home");
   const { t, i18n } = i18next;
 
+  const { asPath } = useRouter();
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
+
+  const URL = `${origin}${asPath}`;
+  const domain = origin;
+
+
   return (
     <>
       <Head>
         <title>{t("head-title")}</title>
         <meta name="description" content={t("head-content")} />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Open Graph Protocol */}
+        <meta
+          property="og:title"
+          content={t("head-title")}
+        />
+        <meta
+          property="og:description"
+          content={t("head-content")}
+        />
+        <meta
+          property="og:image"
+          content={domain + "/img/main_img.jpg"}
+        />
+        <meta property="og:url" content={URL} />
+
+        {/* Twitter Card */}
+        <meta
+          name="twitter:title"
+          content={t("head-title")}
+        />
+        <meta
+          name="twitter:description"
+          content={t("head-content")}
+        />
+        <meta
+          name="twitter:image"
+          content={domain + "/img/main_img.jpg"}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Navbar i18next={i18next} />
       <SectionTitle
