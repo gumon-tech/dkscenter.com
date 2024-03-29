7ba8d4f0ba51e24bc4cdb6dc647eaae718803b73
dinotong <dinotongo@gmail.com>
2024-03-29 15:38:25 +0700

    add Redirect

---
+++ pages/course/[courseKey]/schedule/index.js
@@ -0,0 +1,21 @@
+import { Redirect } from "/lib/redirect";
+import courses from "/datas/courses.json";
+
+export const getStaticPaths = () => {
+  const courseKeyList = Object.keys(courses);
+  const paths = [];
+  for (const courseKey of courseKeyList) {
+    paths.push({
+      params: { courseKey: courseKey },
+    });
+  }
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = (context) => {
+  const courseKey = context.params?.courseKey || "";
+  const courseData = courses[courseKey];
+  return { props: { courseData } };
+};
+
+export default Redirect;
