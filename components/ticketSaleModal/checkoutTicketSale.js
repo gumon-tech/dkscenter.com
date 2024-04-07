f79b5548e22a9541a1f71166d2cb82afae6cc36f
dinotong <dinotongo@gmail.com>
2024-04-07 23:31:34 +0700

    add setIsLoading false

--- components/ticketSaleModal/checkoutTicketSale.js
+++ components/ticketSaleModal/checkoutTicketSale.js
@@ -93,6 +93,7 @@ export default function CheckoutTicketSale({
     } catch (error) {
       console.error("Error refreshEmailToken :", error);
     }
+    setIsLoading(false);
   };
 
   if (isLoading)
