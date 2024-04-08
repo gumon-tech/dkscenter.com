ba4b119333f99facb68b99b60037662d492e11a1
dinotong <dinotongo@gmail.com>
2024-04-09 01:28:37 +0700

    add api getTickets

---
+++ utils/getTickets.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// ตรวจสอบว่าตัวแปร API_URL ได้ถูกกำหนดค่าหรือไม่
+if (!process.env.API_URL) {
+  throw new Error("API_URL is not defined in environment variables.");
+}
+
+const API_URL = process.env.API_URL;
+
+export async function getTickets({ courseKey, scheduleKey,discountCode }) {
+  try {
+    const response = await axios.get(
+      `${API_URL}/tickets/courses/${courseKey}/schedules/${scheduleKey}`,
+      {
+        params: {
+          discount_code: discountCode,
+        },
+      }
+    );
+
+    // ตรวจสอบว่าการส่งอีเมล์เสร็จสมบูรณ์และไม่มีข้อผิดพลาดเกิดขึ้น
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Failed to send getTickets.");
+    }
+  } catch (error) {
+    console.error("Error sending getTickets:", error);
+    throw error;
+  }
+}
